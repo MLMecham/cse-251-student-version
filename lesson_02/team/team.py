e32ab2f2eb6f736ac63b04eed5718e14870f7436
MLMecham <141757176+MLMecham@users.noreply.github.com>
2024-09-30 11:42:06 -0600

    finsihed project 2

--- lesson_02/team/team.py
+++ lesson_02/team/team.py
@@ -2,7 +2,7 @@
 Course: CSE 251 
 Lesson: L02 Team Activity
 File:   team.py
-Author: <Add name here>
+Author: Mitchell Mecham
 
 Purpose: Make threaded API calls with the Playing Card API http://deckofcardsapi.com
 
@@ -24,8 +24,17 @@ from cse251 import *
 
 class Request_thread(threading.Thread):
     # TODO - Add code to make an API call and return the results
-    # https://realpython.com/python-requests/
-    pass
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+        # https://realpython.com/python-requests/
+
+    def run(self):
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            self.data = response.json()
+        else:
+            print("Error in request")
 
 class Deck:
 
@@ -38,11 +47,21 @@ class Deck:
     def reshuffle(self):
         print('Reshuffle Deck')
         # TODO - add call to reshuffle
+        url = f'https://deckofcardsapi.com/api/deck/{self.id}/shuffle/?remaining=true'
+        thread = Request_thread(url)
+        data = thread.start()
+        print(data)
+        thread.join()
 
 
     def draw_card(self):
         # TODO add call to get a card
-        pass
+        url = f'https://deckofcardsapi.com/api/deck/{self.id}/draw/?count=2'
+        thread = Request_thread(url)
+        card = thread.start()
+        print(f'card is {card}')
+        
+        
 
     def cards_remaining(self):
         return self.remaining
@@ -61,7 +80,7 @@ if __name__ == '__main__':
     #        team_get_deck_id.py program once. You can have
     #        multiple decks if you need them
 
-    deck_id = 'ENTER ID HERE'
+    deck_id = '51ols4zpwv8z'
 
     # Testing Code >>>>>
     deck = Deck(deck_id)
@@ -70,3 +89,6 @@ if __name__ == '__main__':
         print(f'card {i + 1}: {card}', flush=True)
     print()
     # <<<<<<<<<<<<<<<<<<
+
+    deck.reshuffle()
+    deck.draw_card()
